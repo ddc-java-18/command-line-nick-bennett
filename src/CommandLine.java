aead99638019fa45a6c4a83332ae8e752ba01f1a
nick-bennett <nbennett10@cnm.edu>
2024-05-20 12:01:25 -0600

    Fahrenheit-toCelsius conversion

--- src/CommandLine.java
+++ src/CommandLine.java
@@ -3,9 +3,15 @@ public class CommandLine {
     public static void main(String[] args) {
         
         for (String arg : args) {
-            System.out.println(arg);    
+            double fahrenheit = Double.parseDouble(arg);
+            double celsius = convertF2C(fahrenheit);
+            System.out.printf("F = %.2f; C = %.2f%n", fahrenheit, celsius);    
         }
         
     } 
+
+    private static double convertF2C(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }    
 
 }
